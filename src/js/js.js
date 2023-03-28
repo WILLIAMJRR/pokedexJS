cca5ccfa3bd55ed9b2d5fecc247503274793cbeb
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-03-28 17:57:15 -0300

    pokedex

--- src/js/js.js
+++ src/js/js.js
@@ -2,70 +2,93 @@ document.addEventListener('DOMContentLoaded', () => {
     const pokemonContainer = document.querySelector('.pokemon_container');
     const pokemonInput = document.querySelector('.pokemon_name');
     const pokemonForm = document.querySelector('.pokemon_form');
-    const pokemonData = document.querySelector('.pokemon-data');
+    const suggestionsContainer = document.querySelector(
+        '.suggestions_container',
+    );
     const spinner = document.querySelector('#spinner');
 
-    //Esta función recibe como parámetros el nombre del pokemon que se va a obtener y un valor booleano opcional para indicar si se desea mostrar una ventana modal con información adicional sobre el pokemon
     const getPokemon = (pokemonName) => {
-        let url = '';
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
 
-        //Se utiliza isNaN para determinar si el parámetro pasado es un número o no. Si devuelve true, significa que NO es un número.
-        if (isNaN(pokemonName)) {
-            //Si el parámetro NO es un número, se construye la URL de la API con el nombre del pokemon pasado en minúsculas y eliminando los espacios en blanco al principio y final con .trim()
-            url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
-        } else {
-            //Si el parámetro es un número, se construye la URL de la API utilizando dicho número como identificador del pokemon
-            url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
-        }
-
-        //Se hace una petición a la URL construida utilizando la función fetch(), que es una manera moderna y simplificada de realizar peticiones HTTP
         fetch(url)
-            //El resultado de la petición es procesado en formato JSON utilizando .json()
             .then((res) => res.json())
-            //Una vez obtenido el JSON de la petición, se llama a la función createPokemon() pasándole dicho objeto JSON y el parámetro modal, para que cree un elemento HTML con los datos del pokemon obtenidos y lo muestre en pantalla (o en una ventana modal según corresponda)
+
             .then((data) => {
                 createPokemon(data);
+
                 pokemonForm.reset();
 
-                //Cuando se haya terminado de crear el pokemon en pantalla, se ocultará un spinner que estaba animando mientras se cargaban los datos
                 spinner.style.display = 'none';
             })
-            //Si hubo algún error en la petición, se informa en consola con .catch()
+
             .catch((err) => console.log(err));
     };
 
-    //evento del formulario
-    //evento del formulario
     pokemonForm.addEventListener('submit', (event) => {
-        event.preventDefault(); // evitar recarga por defecto de la página
+        event.preventDefault();
 
-        // Eliminar las cartas existentes en pantalla
-        const existingCards = document.querySelectorAll('.flip-card');
-        // El código presentado elimina todas las cartas existentes en pantalla y sirve para asegurarse de que solo aparezca la información del pokemon más reciente buscado por el usuario.
-        // Primero, se utiliza document.querySelectorAll para seleccionar todos los elementos con la clase .flip-card, que son las cartas que se crean cuando se busca un nuevo Pokemon con el formulario. Luego se utiliza Array.from para convertir esa selección NodeList (un tipo especial de colección de elementos HTML), que no tiene acceso al método forEach.
-        // A continuación, se utiliza forEach para iterar sobre cada carta encontrada y se usa el método remove() para eliminarla del documento HTML.
-        // De esta manera, después de eliminar las cartas antiguas, la función createPokemon puede crear una nueva carta para el nuevo Pokemon que el usuario busca utilizando el formulario.
-
-        Array.from(existingCards).map((card) => card.remove());
         const pokemonName = pokemonInput.value.toLowerCase().trim();
         if (pokemonName === '') return; // no hacer nada si se envía un nombre vacío
+        const existingCards = document.querySelectorAll('.flip-card');
+
+        Array.from(existingCards).map((card) => card.remove());
         getPokemon(pokemonName);
     });
 
-    //recorrer los pokemones se encarga de llamar iteradas veces la funcion get pokemon
-    //paginancion antes estaba number como parametro
+    //buscar pokemon por sugerencia
+    pokemonInput.addEventListener('input', async () => {
+        const searchTerm = pokemonInput.value.trim().toLowerCase();
+        if (searchTerm.length >= 2) {
+            try {
+                const response = await fetch(
+                    `https://pokeapi.co/api/v2/pokemon?limit=1118`,
+                );
+                const data = await response.json();
+                const matchingPokemon = data.results.filter((pokemon) =>
+                    pokemon.name.startsWith(searchTerm),
+                );
+                const suggestionsHTML = matchingPokemon
+                    .map(
+                        (pokemon) =>
+                            `<div class="suggestion">${pokemon.name}</div>`,
+                    )
+                    .join('');
+                suggestionsContainer.innerHTML = suggestionsHTML;
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            suggestionsContainer.innerHTML = '';
+        }
+    });
+
+    suggestionsContainer.addEventListener('click', (event) => {
+        const clickedSuggestion = event.target;
+        if (!clickedSuggestion.classList.contains('suggestion')) return;
+        const existingCards = document.querySelectorAll('.flip-card');
+
+        Array.from(existingCards).map((card) => card.remove());
+        const selectedPokemon = clickedSuggestion.innerText;
+        getPokemon(selectedPokemon);
+        suggestionsContainer.innerHTML = '';
+    });
+
+    // numero de pokemon
 
     let firstIndex = 1;
     let lastIndex = 7;
     let arrayPokemon = [];
 
-    const previous = document.getElementById('previous');
+    const addOpacity = () => {
+        const previous = document.getElementById('previous');
+        previous.classList.add('btn_opacity');
+    };
 
     previous.addEventListener('click', () => {
-        if (firstIndex <= 1) {
-            previous.classList.add('btn_opacity');
+        if (firstIndex <=1) {
+            addOpacity();
         } else {
-            firstIndex -= 6;
+            firstIndex -= 8;
             getPokemons(firstIndex, lastIndex);
             removerElementosRepetidos(pokemonContainer);
         }
@@ -77,7 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (firstIndex < arrayPokemon) {
             next.classList.add('btn_opacity');
         } else {
-            firstIndex += 6;
+            firstIndex += 8;
             previous.classList.remove('btn_opacity');
             getPokemons(firstIndex, lastIndex);
             removerElementosRepetidos(pokemonContainer);
@@ -91,16 +114,14 @@ document.addEventListener('DOMContentLoaded', () => {
             getPokemon(i);
         }
     };
-    // funcion para remover los pokemones al pasar la pagina
+
     function removerElementosRepetidos(parent) {
-        // mientras en el pokemonContainer que es el contenedor donde se renderiza los pokemones hay una tarjeta se quita y se agrega las nuevas
         while (parent.firstChild) {
             parent.removeChild(parent.firstChild);
         }
     }
     getPokemons(firstIndex, lastIndex);
 
-    //crear las cartas de los pokemones
     const createPokemon = (pokemon) => {
         const pokemonContainer = document.querySelector('.pokemon_container');
         const modalSearch = document.querySelector('.pokemon_container');
@@ -204,4 +225,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return statsContainer;
     }
+    addOpacity();
 });
